74843bcb5d77c2dcbe11e49beabb5483d6169114
HarryLou <usengjake@outlook.com>
2025-01-09 11:09:30 +0800

    加入astro-shop-view-transitions-master

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+/*
+ * Configs for codrops
+ */
+
+export const tutorialText = "Read the tutorial";
+export const tutorialHref = "https://tympanus.net/codrops/?p=73418";
+
+export const previousText = "All demos";
+export const previousHref = "http://tympanus.net/codrops/demos/";
+
+export const githubText   = "GitHub"
+export const githubHref   = "https://github.com/codrops/astro-shop-view-transitions"
