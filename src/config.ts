afd49d5687fe8921b1dae52b152bc2633ec54b71
HarryLou <usengjake@outlook.com>
2025-01-10 14:26:37 +0800

    修改超連結打開方式

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,11 @@
  * Configs for codrops
  */
 
-export const tutorialText = "Read the tutorial";
-export const tutorialHref = "https://tympanus.net/codrops/?p=73418";
+// export const tutorialText = "Read the tutorial";
+// export const tutorialHref = "https://tympanus.net/codrops/?p=73418";
 
-export const previousText = "All demos";
-export const previousHref = "http://tympanus.net/codrops/demos/";
+export const previousText = "Medium";
+export const previousHref = "https://medium.com/@usengjake";
 
 export const githubText   = "GitHub"
-export const githubHref   = "https://github.com/codrops/astro-shop-view-transitions"
+export const githubHref   = "https://github.com/thegamejake/astro-shop-view-transitions-master"
